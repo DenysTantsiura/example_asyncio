0261b5ba49f3e3904c999354b8fc4b56326e32d4
Denys Tantsiura <ghost0002501@gmail.com>
2023-02-10 12:20:09 +0200

    Added chat and exchange command.

---
+++ chat.py
@@ -0,0 +1,59 @@
+import asyncio
+from datetime import datetime
+import logging
+import websockets
+import names  # генеруватиме випадкове ім'я користувачеві
+from websockets import WebSocketServerProtocol
+from websockets.exceptions import ConnectionClosedOK
+
+from main import main as PBexchange
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Server:
+    clients = set()
+
+    async def register(self, ws: WebSocketServerProtocol):
+        ws.name = names.get_full_name()
+        self.clients.add(ws)
+        logging.info(f'{ws.remote_address} connects')
+
+    async def unregister(self, ws: WebSocketServerProtocol):
+        self.clients.remove(ws)
+        logging.info(f'{ws.remote_address} disconnects')
+
+    async def send_to_clients(self, message: str):
+        if self.clients:
+            [await client.send(message) for client in self.clients]
+
+    async def ws_handler(self, ws: WebSocketServerProtocol):
+        await self.register(ws)
+        try:
+            await self.distrubute(ws)
+        except ConnectionClosedOK:
+            pass
+        finally:
+            await self.unregister(ws)
+
+    async def distrubute(self, ws: WebSocketServerProtocol):
+        async for message in ws:
+            if message.startswith('exchange'):
+                await self.send_to_clients(f'{ws.name}: {message} start on {datetime.now()}\n(Example for run:\texchange 5 USD EUR CHF)\n')
+                message = await self.get_exchange_currency(message) # await PBexchange([])
+                await self.send_to_clients(f"Server: {message}")
+            else:
+                await self.send_to_clients(f"{ws.name}: {message}")
+
+    @staticmethod
+    async def get_exchange_currency(message: str) -> str:
+        return await PBexchange(message.split(' '))
+
+
+async def main():
+    server = Server()
+    async with websockets.serve(server.ws_handler, 'localhost', 8080):
+        await asyncio.Future()  # run forever
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- main.py
+++ main.py
@@ -11,52 +11,61 @@ from asyncduration import async_timed
 from asynclogging import async_logging_to_file
 
 
-logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
+logging.basicConfig(level=logging.DEBUG, format="%(threadName)s %(message)s")
 URL_API_PRIVATBANK_ARCH = 'https://api.privatbank.ua/p24api/exchange_rates?json&date='
 DAY_LIMIT = 10
 
 
 class InterfaceOutput(ABC):
-
     @abstractmethod
     def show_out(self, *args, **kwargs):
-        ...    
+        ...
 
 
 class OutputAnswer(InterfaceOutput):
     """Show filtered result for certain currency."""
+
     @staticmethod
-    def show_out(answers: List[dict], filter_currency: List[str]) -> bool:
-        """Show answer for the user."""
+    def show_out(answers: List[dict], filter_currency: List[str]) -> str:
+        """Give answer to the user."""
         if not answers or not filter_currency:
-            logging.critical(f'Something wrong on server?:\nstatus code {answers}\ncurrency request {filter_currency}')
-            return False
-        
+            answer = f'Something wrong on server?:\nstatus code {answers}\ncurrency request {filter_currency}'
+            logging.critical(answer)
+            return answer
+
         result = []
-        for record in answers:  # {"date":"01.12.2014",...,"exchangeRate":[{...,"currency":"CHF"...},...,{}]
+        for (
+            record
+        ) in (
+            answers
+        ):  # {"date":"01.12.2014",...,"exchangeRate":[{...,"currency":"CHF"...},...,{}]
             if not isinstance(record, dict):
-                logging.critical(f'Invalid server answer, status code:\n{answers}')
-                return False
-            
+                answer = f'Invalid server answer, status code:\n{answers}'
+                logging.critical(answer)
+                return answer
+
             key_day = record['date']
             result_day = {}
             for currency in filter_currency:  # 'EUR', 'USD', ...
                 result_currency = {}
-                for currency_in_record in record['exchangeRate']:  # [{...,"currency":"CHF"...},...,{}]
+                for currency_in_record in record[
+                    'exchangeRate'
+                ]:  # [{...,"currency":"CHF"...},...,{}]
                     if currency_in_record['currency'] == currency:  # "currency":"CHF"
                         result_currency[currency] = {
-                                                'sale': currency_in_record.get('saleRate', None), 
-                                                'purchase': currency_in_record.get('purchaseRate', None)
-                                                }  # result_currency = {'EUR': {...}}
-                        
+                            'sale': currency_in_record.get('saleRate', None),
+                            'purchase': currency_in_record.get('purchaseRate', None),
+                        }  # result_currency = {'EUR': {...}}
+
                         result_day.update(result_currency)
             result.append({key_day: result_day})
 
-        logging.info(f'Done:\n\t{result}')
+        answer = f'Done:\n\t{result}'
+        logging.info(answer)
 
-        return True
-    
-    
+        return answer
+
+
 class ClientApplication:
     @async_timed()
     async def consumer(self, uri: str):
@@ -70,62 +79,66 @@ class ClientApplication:
                         # logging.info(f'CHARSET:\t{self.response_charset}')
                         # self.response_cookies = response.cookies
                         result = await response.json()
-                        await async_logging_to_file(f'\n{result}\nreceived: \t\t{datetime.now()}')
+                        await async_logging_to_file(
+                            f'\nExchange currency information received: \t\t{datetime.now()}'
+                        )
                         return result
-                    
+
                     else:
                         logging.error(f'Request error, status code:\n{response.status}')
                         return response.status
-                    
+
             except aiohttp.ClientConnectorError as err:
                 print(f'Connection error: {uri}', str(err))
-    
-    
+
+
 class PrivatBankExchangeRate(ClientApplication):
     """Main class get the PrivatBank exchange rate."""
-    def __init__(self) -> None:
 
+    def __init__(self, sys_argv: list) -> None:
         self.query_data = datetime.now().strftime('%d.%m.%Y')
         # logging.info(f'ToDay:\t{self.query_data}')
         try:
-            # logging.info(f'Start with first parameter:\n{sys.argv[1]}')
-            self.a_certain_past_day = int(sys.argv[1]) if 0 < int(sys.argv[1]) < DAY_LIMIT else DAY_LIMIT
+            # logging.info(f'Start with first parameter:\n{sys_argv[1]}')
+            self.a_certain_past_day = (
+                int(sys_argv[1]) if 0 < int(sys_argv[1]) < DAY_LIMIT else DAY_LIMIT
+            )
 
         except (IndexError, TypeError):
             self.a_certain_past_day = 1
-        
-        # self.uri = [f'''{URL_API_PRIVATBANK_ARCH}{(datetime.now()-timedelta(days=step)).strftime('%d.%m.%Y')}'''\
-        #  for step in range(self.a_certain_past_day)]
-        self.uri = []
-        for day in range(self.a_certain_past_day):
-            query: datetime = datetime.now()-timedelta(days=day)
-            self.uri.append(f'''{URL_API_PRIVATBANK_ARCH}{query.strftime('%d.%m.%Y')}''')
-        
+
+        self.uri = [
+            f'''{URL_API_PRIVATBANK_ARCH}{(datetime.now()-timedelta(days=step)).strftime('%d.%m.%Y')}'''
+            for step in range(self.a_certain_past_day)
+        ]
+
         try:
-            # logging.info(f'Other parameters:\n{sys.argv[2:]}')
-            self.currency_list = sys.argv[2:] or ['EUR', 'USD']
+            # logging.info(f'Other parameters:\n{sys_argv[2:]}')
+            self.currency_list = sys_argv[2:] or ['EUR', 'USD']
 
         except IndexError:
             self.currency_list = ['EUR', 'USD']
 
-        print(f'{datetime.now()}\nReceive PrivatBank exchange rate for the last {self.a_certain_past_day} day(s)...\n')
+        print(
+            f'{datetime.now()}\nReceive PrivatBank exchange rate for the last {self.a_certain_past_day} day(s)...\n'
+        )
 
     async def get_exchange(self) -> tuple:
-        
         tasks = [asyncio.create_task(self.consumer(uri)) for uri in self.uri]
         answer = await asyncio.gather(*tasks, return_exceptions=True) if tasks else None
 
         return answer, self.currency_list
-  
+
 
 @async_timed()
-async def main() -> None:
+async def main(sys_argv: list) -> None:
     """Run application."""
-    client = PrivatBankExchangeRate()
+    client = PrivatBankExchangeRate(sys_argv)
     server_answer, filter_currency = await client.get_exchange()
-    OutputAnswer.show_out(server_answer, filter_currency)
     
+    return OutputAnswer.show_out(server_answer, filter_currency)
 
-if __name__ == "__main__":
-    print(f'{datetime.now()}\nExample for run:\tpython main.py 5 USD EUR CHF\n')
-    asyncio.run(main())
+
+if __name__ == '__main__':
+    logging.info(f'{datetime.now()}\nExample for run:\tpython main.py 5 USD EUR CHF\n')
+    asyncio.run(main(sys.argv))
